7823f2556ba09622c818e7cc5bc46f606ceb1813
Shashwat <rawatshashwat191@gmail.com>
2026-05-08 20:17:11 +0530

    Update Android BASE_URL to live Render backend

--- android/app/src/main/java/com/example/aistudyassistant/utils/Constants.java
+++ android/app/src/main/java/com/example/aistudyassistant/utils/Constants.java
@@ -1,7 +1,7 @@
 package com.example.aistudyassistant.utils;
 
 public class Constants {
-    public static final String BASE_URL = "https://your-render-url.onrender.com/";
+    public static final String BASE_URL = "https://vansh-project-6vls.onrender.com/";
     public static final String TOKEN_KEY = "jwt_token";
     public static final String USER_NAME_KEY = "user_name";
     public static final String USER_EMAIL_KEY = "user_email";
